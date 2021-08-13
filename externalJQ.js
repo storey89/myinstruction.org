f43cf55c78b93fb812f8743872aa17dbd8a71b9f
storey89 <paddystorey@gmail.com>
2021-08-13 21:10:16 +0100

    Add files via upload

---
+++ externalJQ.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$("#begin").click(function(){ // If user selects super burger send a confirm to warm them.
+		alert("Hello\nHow are you?");
+		$('.level').text("Hello World");
+		$('.type').text("Hello World");
+		$('.ore').text("Hello World");
+		//confirm("Finnegan’s says, by clicking continue you're stating that you have understood the dangers involved with this burger and you are argreeing to continue.");
+	});
+	$(".intermedio").click(function(){ // If user selects super burger send a confirm to warm them.
+	});
+	$(".Avanzato").click(function(){ // If user selects super burger send a confirm to warm them.
+	});
+	$(".Conversatio").click(function(){ // If user selects super burger send a confirm to warm them.
+	});
+	$(".scrivere").click(function(){ // If user selects super burger send a confirm to warm them.
+	});
+	$(".narrazione").click(function(){ // If user selects super burger send a confirm to warm them.
+	});
+	$(".application").click(function(){ // If user selects super burger send a confirm to warm them.
+	});
+});
---
+++ gogo.php
@@ -0,0 +1,45 @@
+<?php
+	$name=$_POST['name'];
+	$visitor_email=$_POST['email'];
+	$message=$_POST['message'];
+	$email_form='info@myinstruction.org';
+	$email_subject="New form submission";
+	$email_body="You have recieved a new message from the user $name. \n
+	Here is the message: \n $message";
+
+  $to="info@myinstruction.org";
+  $headers="From: $email_from \r\n";
+  $headers="Reply-To: $visitor_email \r\n";
+  mail($to,$email_subject,$email_body,$headers);
+	function IsInjected($str)
+	{
+	    $injections = array('(\n+)',
+	           '(\r+)',
+	           '(\t+)',
+	           '(%0A+)',
+	           '(%0D+)',
+	           '(%08+)',
+	           '(%09+)'
+	           );
+	               
+	    $inject = join('|', $injections);
+	    $inject = "/$inject/i";
+	    
+	    if(preg_match($inject,$str))
+	    {
+	      return true;
+	    }
+	    else
+	    {
+	      return false;
+	    }
+	}
+
+	if(IsInjected($visitor_email))
+	{
+	    echo "Bad email value!";
+	    exit;
+	}
+?>
+
+
