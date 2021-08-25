dfca72f6979079af0933c5f27c4b544481462704
storey89 <paddystorey@gmail.com>
2021-08-25 19:03:17 +0100

    Update gogo.php

--- gogo.php
+++ gogo.php
@@ -3,8 +3,7 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$formcontent="From: $name \n Message: $message";
-	$recipient = "
-x20200439@student.ncirl.ie";
+	$recipient = "x20200439@student.ncirl.ie";
 	$subject = "Contact Form";
 	$mailheader = "From: $email \r\n";
 	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
