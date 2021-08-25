9ff6d8c8dc9d294fa5dc9791ab9b08cb4a5ce557
storey89 <paddystorey@gmail.com>
2021-08-25 18:41:47 +0100

    Add files via upload

--- gogo.php
+++ gogo.php
@@ -3,42 +3,10 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$formcontent="From: $name \n Message: $message";
-	$recipient = "info@myinstruction.org";
+	$recipient = "
+x20200439@student.ncirl.ie";
 	$subject = "Contact Form";
 	$mailheader = "From: $email \r\n";
 	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 	echo "Thank You!";
 ?>
-
-
-	/*function IsInjected($str)
-	{
-	    $injections = array('(\n+)',
-	           '(\r+)',
-	           '(\t+)',
-	           '(%0A+)',
-	           '(%0D+)',
-	           '(%08+)',
-	           '(%09+)'
-	           );
-	               
-	    $inject = join('|', $injections);
-	    $inject = "/$inject/i";
-	    
-	    if(preg_match($inject,$str))
-	    {
-	      return true;
-	    }
-	    else
-	    {
-	      return false;
-	    }
-	}
-
-	if(IsInjected($visitor_email))
-	{
-	    echo "Bad email value!";
-	    exit;
-	}*/
-
-
