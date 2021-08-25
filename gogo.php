9665291bdb978468301338e01d4f02146b8113db
storey89 <paddystorey@gmail.com>
2021-08-25 17:48:31 +0100

    Add files via upload

--- gogo.php
+++ gogo.php
@@ -1,23 +1,13 @@
 <?php
-	$errors = '';
-	$myemail = 'info@myinstruction.org';//<-----Put Your email address here.
-		if(empty($_POST['name'])  || 
-   			empty($_POST['email']) || 
-   			empty($_POST['message']))
-			{
-    			$errors .= "\n Error: all fields are required";
-			}
-
-			$name = $_POST['name']; 
-			$email_address = $_POST['email']; 
-			$message = $_POST['message']; 
-
-			if (!preg_match(
-			"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-			$email_address))
-			{
-			    $errors .= "\n Error: Invalid email address";
-			}
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$formcontent="From: $name \n Message: $message";
+	$recipient = "info@myinstruction.org";
+	$subject = "Contact Form";
+	$mailheader = "From: $email \r\n";
+	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	echo "Thank You!";
 ?>
 
 
